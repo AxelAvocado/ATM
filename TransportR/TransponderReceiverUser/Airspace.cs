2f2c628a5cf1c85fe01f71af4cfcc8611d6477df
zabih <44346668+mrching7@users.noreply.github.com>
2019-10-23 12:57:53 +0200

    tilføjet filer til transportR

---
+++ TransportR/TransponderReceiverUser/Airspace.cs
@@ -0,0 +1,18 @@
+﻿namespace TransponderReceiverUser
+{
+    public class Airspace
+    {
+        private int x, y, z;
+
+        public Airspace(int x_ = 80000, int y_ = 80000, int z_ = 20000)
+        {
+            x = x_;
+            y = y_;
+            z = z_;
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int Z { get => z; set => z = value; }
+    }
+}
