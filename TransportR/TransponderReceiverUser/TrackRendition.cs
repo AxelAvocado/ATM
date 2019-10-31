8e9e90300b78d13e133d3b1be785b499101426e2
Saka <m.m.hagler@outlook.dk>
2019-10-31 10:40:07 +0100

    Tilføjet print af updates list

---
+++ TransportR/TransponderReceiverUser/TrackRendition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransponderReceiverApplication;
+
+namespace TransponderReceiverUser
+{
+    class TrackRendition
+    {
+       
+        public void PrintPlanes(List<AirplaneData> Planes)
+        {
+            CalculateAirplaneData Planelist = new CalculateAirplaneData();
+            Planes = Planelist.AirplanesUpdated;
+
+
+            foreach (var item in Planes)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
