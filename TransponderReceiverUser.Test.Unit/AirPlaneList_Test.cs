c13d91f48fa50b7bbd19f497d8cdd594d3303bc2
zabih <44346668+mrching7@users.noreply.github.com>
2019-11-01 12:37:47 +0100

    tilføjet test for airplane list samt tilføjet interface

---
+++ TransponderReceiverUser.Test.Unit/AirPlaneList_Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using TransponderReceiver;
+using TransponderReceiverApplication;
+
+namespace TransponderReceiverUser.Test.Unit
+{
+    public class AirPlaneList_Test
+    {
+        //laver unittest til AirPlaneList
+        private AirplanesList UUT;
+
+        [SetUp]
+        public void Setup()
+        {
+            UUT=new AirplanesList();
+        }
+
+
+        //tester AddToList fra AirPlaneList med at give den input parameter og se hvorvidt det er blevet indsat 
+
+        [Test]
+        public void AddToListTest()
+        {
+            AirplaneData IairPlaneData = Substitute.For<AirplaneData>();
+            IairPlaneData.Tag.Returns("Fly1");
+
+            UUT.AddToList(IairPlaneData);
+
+            Assert.That(UUT.AirplaneDataList, Is.EqualTo("fly1"));
+
+        }
+
+
+
+    }
+}
