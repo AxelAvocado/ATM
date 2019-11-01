964660d99667d1d460fcebc252ab8921560ea024
AxelAvocado <abdallah@ajjawi.dk>
2019-11-01 12:52:16 +0100

    Created test for calculatespeed

---
+++ TransponderReceiverUser.Test.Unit/CalculateAirplaneData_Test.cs
@@ -0,0 +1,30 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransponderReceiverUser.Test.Unit
+{
+    class CalculateAirplaneData_Test
+    {
+        private ITransponderReceiverClient _fakeTransponderReceiverClient;
+        private CalculateAirplaneData _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fakeTransponderReceiverClient = Substitute.For<ITransponderReceiverClient>();
+            _uut = new CalculateAirplaneData(_fakeTransponderReceiverClient);
+        }
+
+        // Test: Beregning af hastighed
+        [Test]
+        public void CalculateSpeed(AirplaneData Airplane, AirplaneData AirplaneUpdated)
+        {
+
+        }
+    }
+}
